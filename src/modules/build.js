a8b896e8054ecdf492dca1b41890ce2829354db5
aismeg <iaskja20@gmai.com>
2024-03-27 17:18:00 +0100

    tooltip added

--- src/modules/build.js
+++ src/modules/build.js
@@ -4,17 +4,19 @@ const build = (origin, translate) => {
     let countItems = document.querySelector(".list").childElementCount + 1;
 
     list.innerHTML += `
-    <div class="li-wrapper">
-    <div class='li'>
-    <span class="key">${countItems}</span>
-    <span class="span">${origin}</span>
-    </div>
-    <div class='li'>
-    <span class="span">${translate}</span>
-    <img class="delete" src="./img/icon/delete.svg" alt="delete">
-    </div>
-    </div>
-    `
+        <div class="li-wrapper">
+        <div class='li li-orgn'>
+        ${origin.length > 7 ? `<span class="tooltip">${origin}</span>` : null}
+        <span class="key">${countItems}</span>
+        <span class="span">${origin}</span>
+        </div>
+        <div class='li li-trns'>
+        ${origin.length > 7 ? `<span class="tooltip">${translate}</span>` : null}
+        <span class="span">${translate}</span>
+        <img class="delete" src="./img/icon/delete.svg" alt="delete">
+        </div>
+        </div>
+        `
 }
 
 export default build;
